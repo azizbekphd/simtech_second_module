f38f68503a2bec41ed98a3d5f9745578a93d339b
Azizbek <azizbekphd@gmail.com>
2022-12-12 10:10:32 +0500

    Add-on init

---
+++ app/addons/departments/func.php
@@ -0,0 +1,14 @@
+<?php
+/*
+* © 2022 CS-Cart.ie
+* 
+* PLEASE READ THE FULL TEXT  OF THE SOFTWARE  LICENSE   AGREEMENT  
+* IN  THE "LICENSE.txt" FILE PROVIDED WITH THIS DISTRIBUTION PACKAGE. 
+* 
+ * @website: www.cs-cart.ie
+*  
+*/
+
+if ( !defined('BOOTSTRAP') ) { die('Access denied'); }
+
+use Tygh\Registry;
