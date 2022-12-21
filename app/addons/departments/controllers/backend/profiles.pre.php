10931effd0d4245d9d4bdcd812ce5d9b49b3f1bf
Azizbek <azizbekphd@gmail.com>
2022-12-21 20:26:16 +0500

    isolated precontroller for profiles

--- app/addons/departments/controllers/backend/profiles.pre.php
+++ app/addons/departments/controllers/backend/profiles.pre.php
@@ -2,20 +2,23 @@
 
 use Tygh\Registry;
 
-$auth = & Tygh::$app['session']['auth'];
+if (isset($_REQUEST["for_departments"])) {
 
-if ($mode == "picker") {
-    $params = $_REQUEST;
-    $params['skip_view'] = 'Y';
-
-    list($users, $search) = fn_get_users($params, $auth, Registry::get('settings.Appearance.admin_elements_per_page'));
-    Tygh::$app['view']->assign('users', $users);
-    Tygh::$app['view']->assign('search', $search);
-
-    Tygh::$app['view']->assign('countries', fn_get_simple_countries(true, CART_LANGUAGE));
-    Tygh::$app['view']->assign('states', fn_get_all_states());
-    Tygh::$app['view']->assign('usergroups', fn_get_usergroups(array('status' => array('A', 'H')), CART_LANGUAGE));
-
-    Tygh::$app['view']->display('pickers/users/picker_contents.tpl');
-    exit;
+    $auth = & Tygh::$app['session']['auth'];
+    
+    if ($mode == "picker") {
+        $params = $_REQUEST;
+        $params['skip_view'] = 'Y';
+    
+        list($users, $search) = fn_get_users($params, $auth, Registry::get('settings.Appearance.admin_elements_per_page'));
+        Tygh::$app['view']->assign('users', $users);
+        Tygh::$app['view']->assign('search', $search);
+    
+        Tygh::$app['view']->assign('countries', fn_get_simple_countries(true, CART_LANGUAGE));
+        Tygh::$app['view']->assign('states', fn_get_all_states());
+        Tygh::$app['view']->assign('usergroups', fn_get_usergroups(array('status' => array('A', 'H')), CART_LANGUAGE));
+    
+        Tygh::$app['view']->display('pickers/users/picker_contents.tpl');
+        exit;
+    }
 }
